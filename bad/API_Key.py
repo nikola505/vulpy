4c5848996245a1f178006d51373e57e603fb5bcc
nikola505 <141411428+nikola505@users.noreply.github.com>
2025-05-05 19:36:38 +0200

    Update API_Key.py

--- bad/API_Key.py
+++ bad/API_Key.py
@@ -1,5 +1,5 @@
 def connect_to_service():
-    app.config['SECRET_KEY'] = '123aa8a93bdde342c871564a62282af857bda14b3359fde95d0c5e4b321610c1'
+    api_key = '123aa8a93bdde342c871564a62282af857bda14b3359fde95d0c5e4b321610c1'
     print("Connecting to service with key:", api_key)
 
 connect_to_service()
